0ed9896d62e9f62937a59ccc94ec1b230f715cc2
CryingEvil <erwinpurnama567gc@gmail.com>
2022-05-27 12:29:17 +0700

    my first node

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log("Test Erwin");
+let a = 1;
+let b = 2;
+let c = a + b;
+console.log(c);
+
+function main (){
+    kurang(5,7);
+}
+
+function kurang(a,b){
+    console.log(a-b);
+   
+}
+main();
+const d = 1;
+console.log(d);
+
+const kurang = (a,b)=>{
+    console.log(a-b);
+}
