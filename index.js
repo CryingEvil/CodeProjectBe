92a0a75fe52ef586b6132c1b9ba3c29a36e0fcdd
CryingEvil <erwinpurnama567gc@gmail.com>
2022-05-28 22:28:12 +0700

    node model mys sql

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
-console.log("Test Erwin");
-let a = 1;
-let b = 2;
-let c = a + b;
-console.log(c);
 
-function main (){
-    kurang(5,7);
-}
+const express = require('express');
+const app = express();
+const mySql = require('mysql');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const db = mySql.createPool({
+    host: "localhost",
+    user: "root",
+    database: "library"
+})
 
-function kurang(a,b){
-    console.log(a-b);
-   
-}
-main();
-const d = 1;
-console.log(d);
+app.use(cors());
+app.use(bodyParser());
+app.listen(3001, () =>
+    console.log("running on port 3001"));
 
-const kurang = (a,b)=>{
-    console.log(a-b);
-}
+app.get('/', (req, res) => {
+    const sqlState = "INSERT INTO favorite (title) VALUE ('Ini Test Masuk Database pakai server')";
+    db.query(sqlState, (err, rslt) => {
+        if (err) {
+            res.send(err.message);
+        }
+        else {
+            res.send('Berhasil');
+        }
+    })
+});
+
+
+app.post('/save',(req,res)=>{
+    const title = req.body.title;
+    const bookId = req.body.bookId;
+    const sqlState = "INSERT INTO favorite (title,book_id) VALUE ('" + title +"'"+","+"'"+bookId+"')";
+    db.query(sqlState, (err, rslt) => {
+        if (err) {
+            res.send(err.message);
+        }
+        else {
+            res.send('Berhasil');
+        }
+    })
+})
+
